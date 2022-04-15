0623934f9b55cd6b8f82077132b80df2661299ee
dgw <ds257873@naver.com>
2022-04-15 22:50:13 +0900

    조회수 기능 추가

--- src/main/java/com/example/demo5/web/dto/PostsListResponseDto.java
+++ src/main/java/com/example/demo5/web/dto/PostsListResponseDto.java
@@ -14,6 +14,7 @@ public class PostsListResponseDto {
     private String content;
     private String modifiedDate;
     private String boardnumber;
+    private Integer views;
 
     public PostsListResponseDto(Posts entity){
         this.id = entity.getId();
@@ -22,5 +23,6 @@ public class PostsListResponseDto {
         this.content = entity.getContent();
         this.modifiedDate = entity.getModifiedDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd mm:hh"));
         this.boardnumber = entity.getBoardnumber();
+        this.views = entity.getViews();
     }
 }
